645ebb4d9891e1557edc09715b544a87f68a4bc6
Bastian Ilso <hougaard.junior@gmail.com>
2023-03-14 11:48:56 +0100

    utils: loadrawdata.R update to latest version
    fixes issues with importing CSV files.

--- utils/loadrawdata.R
+++ utils/loadrawdata.R
@@ -69,7 +69,6 @@ LoadFromDirectory <- function(dir, event = "Event", sample = "Sample", meta = "M
       add_column(SessionID= NA)
     
   }
-
   dataset <- MergeDatasets(df_meta, df_event, df_sample)
   
   return(dataset)
@@ -90,7 +89,7 @@ PreprocessMeta <- function(dataset_meta) {
 
 MergeDatasets <- function(dataset_meta, dataset_event, dataset_sample) {
   df = data.frame()
-  if(is.na(unique(dataset_meta$SessionID)) ){
+  if(length(na.omit(unique(dataset_meta$SessionID))) < 1){
     dataset_meta = dataset_meta %>% mutate(SessionID = NULL) 
     df = dataset_event %>% bind_rows(dataset_sample) %>% left_join(dataset_meta, by="filename")
   }else {
